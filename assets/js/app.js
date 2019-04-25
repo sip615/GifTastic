2bc34c067325e917759db8a74cbe1baed035d8c1
sip615 <sunilpatel.328@gmail.com>
2019-04-25 14:17:18 -0400

    finish up the code

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,21 +4,21 @@ var input = $("#input");
 var submit = $("#submit");
 var imgBody = $('#imgBody');
 
-
 //Capture the search value when the user clicks submit
     submit.on("click", function(event){
         event.preventDefault();
+        imgBody.empty();
         console.log(input.val());
         var inputValue = input.val();
         getGifs(inputValue);
-        buttons.push(input.val());
+        //clear the input
+        input.val(' ');
     });
 
 //make a GET request to the Giphy API with the value from the search input
 
 var url = 'http://api.giphy.com/v1/gifs/search?q='
 var apiKey = 'BkfiElo6kR2Oe1QyHQSEBAc296u2Dj7m'
-
 
 function getGifs(inputValue){
     $.get(`${url}${inputValue}&api_key=${apiKey}&limit=10`)
@@ -27,19 +27,15 @@ function getGifs(inputValue){
             var imgSrc = response.data[i].images.downsized.url;
             createBox(imgSrc);
         };
-        
     });
 };
-
 const createBox = (imgSrc) => {
     const newImg = $('<img>');
     newImg.attr('src', imgSrc);
     newImg.addClass('img-box');
     imgBody.append(newImg);
 };
-
-
-//The below was coded with Justin during office hours. 
+//The below was coded with Justin during office hours. Attempted to populate buttons based on search inputs, but just couldn't get it to work the way it was supposed to.
 
 /*
 let buttons = ["Johnny Depp","Lucy Liu","Salma Hayak","Chris Hemsworth"];
