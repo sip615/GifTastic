43696bff68f1a8a777cb29dda0b6a5aff8a5e0ea
sip615 <sunilpatel.328@gmail.com>
2019-04-25 14:09:30 -0400

    finishing up the code

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 //Global Variables
 var input = $("#input");
 var submit = $("#submit");
+var imgBody = $('#imgBody');
 
 
 //Capture the search value when the user clicks submit
@@ -10,6 +11,7 @@ var submit = $("#submit");
         console.log(input.val());
         var inputValue = input.val();
         getGifs(inputValue);
+        buttons.push(input.val());
     });
 
 //make a GET request to the Giphy API with the value from the search input
@@ -19,17 +21,38 @@ var apiKey = 'BkfiElo6kR2Oe1QyHQSEBAc296u2Dj7m'
 
 
 function getGifs(inputValue){
-    $.get(url + inputValue + '&api_key=' + apiKey + '&limit=5')
-    .done(function(data){
-        console.log(data);
-    })
-}
+    $.get(`${url}${inputValue}&api_key=${apiKey}&limit=10`)
+    .done(function(response){
+        for(var i = 0 ; i < 10 ; i++){
+            var imgSrc = response.data[i].images.downsized.url;
+            createBox(imgSrc);
+        };
+        
+    });
+};
+
+const createBox = (imgSrc) => {
+    const newImg = $('<img>');
+    newImg.attr('src', imgSrc);
+    newImg.addClass('img-box');
+    imgBody.append(newImg);
+};
 
 
+//The below was coded with Justin during office hours. 
 
+/*
+let buttons = ["Johnny Depp","Lucy Liu","Salma Hayak","Chris Hemsworth"];
 
+buttons.forEach(button => {
+    $("#buttons").append(`<button>${button.toUpperCase()}</button>`)
+});
 
-
+$("button").on("click", () => {
+    let val = $(this).val();
+    getGifs(val);
+});
+*/
 
 
 });//END OF CODE 
